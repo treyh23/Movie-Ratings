4819ffbb72fce68ba0466e064f1f81da42c2bf98
treyh23 <forrest@contempows.com>
2018-03-31 23:35:22 -0700

    added case statement for add, update, display, delete and error

--- script.rb
+++ script.rb
@@ -4,3 +4,16 @@ movies = {
   }
 puts "what's your favorite movie?"
 choice = gets.chomp
+
+case choice
+when "add"
+  puts "added!"
+when "update"
+  puts "updated!"
+when "display"
+  puts "Movies!"
+when "delete"
+  puts "Deleted!"
+else
+  puts "Error!"
+end
